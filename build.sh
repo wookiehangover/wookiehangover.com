5ef27848bdd7368aad88cb4894c8e3d61f3cd947
Sam Breed <sam@quickleft.com>
2016-08-29 20:09:05 -0700

    Change nothing and continue with immaculate consistency

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
 
-PATH=`npm bin`:$PATH
+# Prefix and compress css into docs/
+postcss --use autoprefixer --use postcss-csso style.css -d docs
 
+# Minify and inline all of the HTML files into docs/
 for file in $(ls *.html); do
   echo "Inlining and Minifying $file..."
   sed 's/style.css/.\/docs\/style.css/g' $file | inliner > docs/$file
   echo "🆗 docs/$file written"
 done;
 
+# Copy all other assets to docs/
 cp *.gif *.jpg favicon.ico docs
