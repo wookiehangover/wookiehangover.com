f47c37f467286423c2323c881789d0e7d42a87de
Sam Breed <sam@quickleft.com>
2016-09-13 20:58:51 -0700

    Imagine the piece as a set of disconnected events

--- build/postcss.sh
+++ build/postcss.sh
@@ -2,8 +2,9 @@
 
 # Prefix and compress css into docs/
 postcss \
-  --use postcss-import \
-  --use postcss-extend \
-  --use autoprefixer \
-  --use postcss-csso \
+  --use postcss-import  \
+  --use postcss-extend  \
+  --use autoprefixer    \
+  --use postcss-csso    \
+  --use postcss-url     \
   style.css -d docs
