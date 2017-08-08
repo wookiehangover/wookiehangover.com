3ba4e9159f60267a3e13e67a95820d4c60cd08f7
Sam Breed <samuel.breed@gmail.com>
2017-08-07 23:09:25 -0700

    [skip ci] dumb gitignore

---
+++ docs/_next/f5712d09-ce4a-4c8c-9875-36d61111f8b3/page/writing/static-sites-with-next-js.html/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/writing/static-sites-with-next-js.html', function() {
+            var comp = module.exports=webpackJsonp([6],{502:function(e,t,n){e.exports=n(503)},503:function(e,t,n){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var d=n(11),a=u(d),r=n(3),f=u(r),l=n(24),i=u(l),o=n(25),p=u(o),s=n(28),c=u(s);t.default=function(e){var t={permalink:"writing/undefined",path:"pages/writing/undefined.js",updatedAt:""},n={type:"div",props:{}};return f.default.createElement(i.default,t,f.default.createElement(p.default,(0,a.default)({},n,{components:c.default})))}}},[502]);
+            return { page: comp.default }
+          })
+        
