b417767f698658a4eeeeb61d18076042e883f615
Sam Breed <samuel.breed@gmail.com>
2017-11-10 23:03:18 -0800

    nuts to this

--- next.config.js
+++ next.config.js
@@ -12,14 +12,17 @@ module.exports = {
       Buffer: false
     }
 
+    /*
     if (dev) {
       return config
     }
 
     config.resolve.alias = {
-      'react': 'preact-compat/dist/preact-compat',
-      'react-dom': 'preact-compat/dist/preact-compat'
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat',
+      'preact-compat': 'preact-compat/dist/preact-compat'
     }
+    */
 
     return config
   },
